dacf9fbf29329b5c0f98e0b97e0d4fce8897d191
umur inan <umurinan@gmail.com>
2019-04-20 14:30:25 -0500

    removing unnecessary generated files

--- finalProject_node/finalProject/app.js
+++ finalProject_node/finalProject/app.js
@@ -9,29 +9,22 @@ var usersRouter = require('./routes/users');
 
 var app = express();
 
-const port =3000;
+const port = 3000;
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
-
-app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
 
+//routers
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
+app.use(function (err, req, res, next) {
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
 
@@ -40,8 +33,8 @@ app.use(function(err, req, res, next) {
   res.render('error');
 });
 
-app.listen(port , ()=> {
-console.log('Server is running . . . Port : 3000');
+app.listen(port, () => {
+  console.log('Server is running . . . Port : 3000');
 });
 
 //module.exports = app;
