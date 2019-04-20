08b3ba0a2624f6d1f7df8bf615c4a170f550c513
umur inan <umurinan@gmail.com>
2019-04-20 10:36:26 -0500

    password validator

--- finalProject/src/app/register.component.ts
+++ finalProject/src/app/register.component.ts
@@ -14,10 +14,19 @@ export class RegisterComponent {
   constructor(http: MwaHttpServiceService, private formBuilder: FormBuilder) {
     this.registerForm = formBuilder.group({
       'mail': ['', Validators.compose([Validators.required, Validators.email])],
-      'password': [''],
+      'password': ['', Validators.compose([Validators.required,this.passwordValidator])],
       'firstName': [''],
       'lastName': [''],
     });
+
+    this.registerForm.statusChanges.subscribe(data => { console.log(data) });
+  }
+
+  passwordValidator(control: FormControl): { [s: string]: boolean } {
+    if (control.value.length <6) {
+      return { 'invalid': true };
+    }
+    return null;
   }
 
   onSubmit(): void {
