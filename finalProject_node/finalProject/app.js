09455f238273f3091d7e5997594e44ba46362021
umur inan <umurinan@gmail.com>
2019-04-21 12:28:51 -0500

    login method added

--- finalProject_node/finalProject/app.js
+++ finalProject_node/finalProject/app.js
@@ -20,7 +20,7 @@ app.use(express.urlencoded({ extended: false }));
 
 //routers
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/api/users', usersRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
--- finalProject_node/finalProject/routes/users.js
+++ finalProject_node/finalProject/routes/users.js
@@ -5,7 +5,19 @@ const db = require('../db/DbHelper.js');
 
 const collectionName = 'user';
 
-/* GET users listing. */
+
+router.post('/login', function (req, res, next) {
+  var mail = req.body.mail;
+  var password = req.body.password;
+  //console.log(mail + ' ' + password);
+  db.getConnection(collectionName).then(data => {
+    data.findOne({ 'mail': mail, 'password': password }).then(userInDb => {
+      res.json(userInDb);
+    });
+  });
+});
+
+
 router.get('/', function (req, res, next) {
   db.getConnection(collectionName).then(data => { data.find({}).toArray((err, data) => { res.json(data) }) })
 });
