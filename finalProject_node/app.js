e34b54abab3f80666053aefeeace255da98e8b0b
gTuugii <tuugii83@gmail.com>
2019-04-21 00:18:03 -0500

    Update app.js

--- finalProject_node/app.js
+++ finalProject_node/app.js
@@ -3,6 +3,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var cors = require('cors');
 
 const MongoClient = require('mongodb').MongoClient;
 const client = new MongoClient('mongodb+srv://test:test@mongodb-0besx.mongodb.net/mwa', { useNewUrlParser: true });
@@ -39,6 +40,7 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(cors());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
